ce0fc981dbd2141546f648a59a9f10e4b42face4
Philip <philip284@gmail.com>
2015-03-06 12:21:16 -0500

    Added RTIMULib heading stuff
    Added RTIMULib heading stuff

--- app.js
+++ app.js
@@ -4,24 +4,87 @@ var app = express();
 
 app.use(cors());
 
-var mpu9150 = require('mpu9150');
+// Switched to RTIMULib
+//var mpu9150 = require('mpu9150');
+
 var MS5803 = require('ms5803_rpi');
 
 // Instantiate and initialize. 
-var mpu = new mpu9150();
-mpu.initialize();
+//var mpu = new mpu9150();
+//mpu.initialize();
 
 var sensor = new MS5803({address: 0x76, device: '/dev/i2c-1'});
 
 var temp;
 var mbar;
 
+var imudata = {
+  time: 0,
+  roll: 0,
+  pitch: 0,
+  yaw: 0,
+  status: 0
+};
+
+var rovdata = {
+	heading: 0,
+	pitch: 0,
+	roll: 0,
+	mbar: 0,
+	temp: 0
+};
+
+//MPU9150
+var PORT = 32000;
+var HOST = '127.0.0.1';
+var dgram = require('dgram');
+var imuserver = dgram.createSocket('udp4');
+
+imuserver.on('listening', function () {
+    var address = imuserver.address();
+//    console.log('Listening for IMU data on: ' + address.address + ":" + address.port);
+});
+
+imuserver.on('message', function (message, remote) {
+    imustring = message.toString().split(' ');
+
+    for(var i=0; i<imustring.length;i++) imustring[i] = +imustring[i];
+
+    imudata.time   = imustring[0];
+    imudata.pitch  = +imustring[1].toFixed(2);
+    imudata.roll   = +imustring[2].toFixed(2);
+    imudata.yaw    = +imustring[3].toFixed(2);
+    if (new Date().getTime() - imudata.time < 15) {
+       imudata.status = 'OK';
+    } else {
+       imudata.status = 'NOK';
+    }
+    if (imudata.roll < 0) {
+            rovdata.roll = Math.floor((imudata.roll + 360)*100)/100;
+    } else {
+            rovdata.roll =  Math.floor((imudata.roll)*100)/100;
+    }
+    if (imudata.pitch < 0) {
+        rovdata.pitch = Math.floor((imudata.pitch + 360)*100)/100;
+    } else {
+        rovdata.pitch = Math.floor((imudata.pitch)*100)/100;
+    }
+    if (imudata.yaw < 0) {
+        rovdata.heading = imudata.yaw + 360;
+    } else {
+        rovdata.heading = imudata.yaw;
+    }
+    rovdata.status = imudata.status;
+
+    //console.log(imudata);
+});
+
+imuserver.bind(PORT, HOST);
+
 updateDepth = function() {
   sensor.read(function (err, data) {
-    temp = data.temperature;
-    mbar = data.pressure;
-    //console.log(temp);
-    //console.log(mbar);
+    rovdata.temp = data.temperature;
+    rovdata.mbar = data.pressure;
   });
   setTimeout(updateDepth, 25);
 };
@@ -33,19 +96,14 @@ app.get('/', function(req, res) {
 });
 
 app.get('/compass', function(req, res) {
-  if (mpu.testConnection()) {
-    res.send(mpu.getMotion9() + '');
-  } else
-  {
-    res.send('Compass Not On');
-  }
+  res.send('' + rovdata.heading + ',' + rovdata.pitch + ',' + rovdata.roll);
 });
 
 app.get('/depth', function(req, res) {
-  res.send(''+ temp + ',' + mbar + '');
+  res.send(''+ rovdata.temp + ',' + rovdata.mbar + '');
 });
 
-var server = app.listen(5678, function() {
+var server = app.listen(80, function() {
   var host = server.address().address;
   var port = server.address().port;
   
