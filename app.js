72522fd5697839cc1b34a070a629fa7c98162b8b
Philip <philip284@gmail.com>
2015-03-26 08:49:08 -0400

    ip

--- app.js
+++ app.js
@@ -106,7 +106,7 @@ app.get('/depth', function(req, res) {
 
 app.get('/', function(req, res) {
     res.render('pages/index', {
-        IpAddr: ip.address()
+        IpAddr: req.getLocalAddr())
     });
 });
 
